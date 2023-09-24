63e281f1f2e53ae0a81e8e6f8894e0f9ebde8aaa
labvm <labvm@labvm.localdomain>
2023-09-23 20:05:58 -0400

    adding proxmox deploy templates

---
+++ lib9/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = var.region
+
+    default_tags {
+        tags = {
+          hashicorp-learn = "refresh"
+        }
+    }
+}
+
+
+data "aws_ami" "amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Learn Refresh"
+  }
+}
+
